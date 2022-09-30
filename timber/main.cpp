64211ccf6108dc44a8848cdc1d07b14cdd9cc873
Librarian423 <youg4231998@naver.com>
2022-09-30 19:11:33 +0900

    main 분기 알고리즘 작성

--- timber/main.cpp
+++ timber/main.cpp
@@ -77,13 +77,12 @@ using namespace sf;
 
 int main()
 {
-    VideoMode vm(X, Y);
-    RenderWindow window(vm, "Timber!!", Style::Default);
-    Vector2u windowSize = window.getSize();
-
-    //
+    //Vector2u windowSize = window.getSize();
+	VideoMode vm(1920, 1080);
+	RenderWindow window(vm, "timber", Style::Default);
+	list<SpriteGameObject*> gameObjectList;
     auto resourceMgr = ResourceManager::GetInstance();
-    list<SpriteGameObject*> gameObjectList;
+    
     gameObjectList.push_back(new SpriteGameObject(*resourceMgr->GetTexture("graphics/background.png")));
     //
 
@@ -96,12 +95,15 @@ int main()
     messageText.setCharacterSize(75);
     messageText.setFillColor(Color::White);
     messageText.setString("Press Enter to start!");
+    Vector2u size = window.getSize();
+    // messageText.setPosition(size.x * 0.5f, size.y * 0.5f);
     //
     Text menuText;
     menuText.setFont(font);
     menuText.setCharacterSize(75);
     menuText.setFillColor(Color::White);
     menuText.setString("Menu");
+    menuText.setPosition(size.x * 0.5f, size.y * 0.5f);
     //
 
     Clock clock;
@@ -109,8 +111,7 @@ int main()
     bool PlayGame = false;
     bool Title = true;
     bool run = true;
-    //SceneManager sceneManager;
-    //sceneManager.Init();
+    int gamemode = 0;
 
     while ( window.isOpen() )
     {
@@ -181,17 +182,20 @@ int main()
             }
             if ( InputManager::GetKeyDown(Keyboard::Key::Up) )
             {
-
+                gamemode = 1;
             }
             if ( InputManager::GetKeyDown(Keyboard::Key::Down) )
             {
-
+                gamemode = 2;
             }
             //Menu()
-            /*if ( Keyboard::isKeyPressed(Keyboard::Return) )
+            if ( Keyboard::isKeyPressed(Keyboard::Return) && gamemode != 0 )
             {
 
-            }*/
+                SelectMenu = false;
+                PlayGame = true;
+            }
+
 
             float deltaTime = dt.asSeconds();
 
@@ -208,25 +212,73 @@ int main()
             {
                 go->Draw(window);
             }
+            if ( gamemode == 1 )
+            {
+                menuText.setString("1P");
+            }
+            else if( gamemode == 2 )
+            {
+                menuText.setString("2P");
+            }
+            else
+            {
+                menuText.setString("Menu");
+            }
             window.draw(menuText);
             window.display();
         }
 
         //Game Scene
-        //while ( PlayGame )
-        //{
-        //    if ( Keyboard::isKeyPressed(Keyboard::Escape) )
-        //    {
-        //        PlayGame = false;
-        //        break;
-        //    }
-        //    if ( Keyboard::isKeyPressed(Keyboard::Return) )
-        //    {
-        //        cout << "Game: " << TestSelection << endl;
-        //    }
-        //    PlayGame = false;
-        //    //Game()
-        //}       
+        if( PlayGame && (gamemode == 1 || gamemode == 2))
+        {
+            if ( gamemode == 1 )
+			{
+				Tree* tree = new Tree(RMI->GetTexture("graphics/tree.png"), gamemode, 1);
+				tree->SetPosition({ 960, 900 });
+				gameObjectList.push_back(tree);
+				Player* player1 = new Player(RMI->GetTexture("graphics/player_green.png"), gamemode, 1, tree->GetPosition());
+				gameObjectList.push_back(player1);
+			}
+			else
+			{
+				Tree* tree1 = new Tree(RMI->GetTexture("graphics/2Ptree.png"), gamemode, 1);
+				tree1->SetPosition({ 480, 900 });
+				gameObjectList.push_back(tree1);
+				Tree* tree2 = new Tree(RMI->GetTexture("graphics/2Ptree.png"), gamemode, 2);
+				tree2->SetPosition({ 1440, 900 });
+				gameObjectList.push_back(tree2);
+				Player* player1 = new Player(RMI->GetTexture("graphics/player_green.png"), gamemode, 1, tree1->GetPosition());
+				gameObjectList.push_back(player1);
+				Player* player2 = new Player(RMI->GetTexture("graphics/player_red.png"), gamemode, 2, tree2->GetPosition());
+				gameObjectList.push_back(player2);
+			}
+            for ( auto i : gameObjectList )
+			{
+				i->Init();
+			}
+            while ( PlayGame )
+            {
+                Time dt = clock.restart(); //이전 업데이트 시간과 현재 업데이트 시간 차이 기록
+
+                Event ev;
+                InputManager::ClearInput();
+                while ( window.pollEvent(ev) )
+                {
+                    InputManager::UpdateInput(ev);
+                }
+                float deltaTime = dt.asSeconds(); //isPause ? 0.f : dt.asSeconds();
+                for ( auto i : gameObjectList )
+                {
+                    i->Update(deltaTime);
+                }
+                for ( auto i : gameObjectList )
+                {
+                    i->Draw(window);
+                }
+                window.display();
+            }
+           
+        }       
     }
     for ( auto go : gameObjectList )
     {
