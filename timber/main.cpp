72928218a91f3bbd79cfdccff6497f8c06fcda5b
Librarian423 <youg4231998@naver.com>
2022-10-01 14:50:46 +0900

    charcter select

--- timber/main.cpp
+++ timber/main.cpp
@@ -21,65 +21,48 @@ int main()
    
 	VideoMode vm(1920, 1080);
 	RenderWindow window(vm, "timber", Style::Default);
-<<<<<<< HEAD
-	list<SpriteGameObject*> gameObjectList;
-    auto resourceMgr = ResourceManager::GetInstance();
-    gameObjectList.push_back(new SpriteGameObject(*resourceMgr->GetTexture("graphics/background.png")));
-    //
-=======
     list<SpriteGameObject*> gameObjectList;
     gameObjectList.push_back(new SpriteGameObject(RMI->GetTexture("graphics/background.png")));
->>>>>>> 17c2a126f8e80abbdbd4babb06a339e066adf603
-
+    Vector2u size = window.getSize();
+    
+    //charcter Menu
+    list<SpriteGameObject*> charcterList;
+    charcterList.push_back(new SpriteGameObject(RMI->GetTexture("graphics/player_green.png"), Vector2f(300, 500)));
+    charcterList.push_back(new SpriteGameObject(RMI->GetTexture("graphics/player_red.png"), Vector2f(900, 500)));
+    charcterList.push_back(new SpriteGameObject(RMI->GetTexture("graphics/player_yellow.png"), Vector2f(1500, 500)));
+    
+   
+    //UI
     UIManager um(RMI->GetFont("fonts/KOMIKAP_.ttf"));
-
-<<<<<<< HEAD
-    Text messageText;
-    Text titleText;
-    messageText.setFont(font);
-    titleText.setFont(font);
-    messageText.setCharacterSize(75);
-    titleText.setCharacterSize(225);
-    messageText.setFillColor(Color::White);
-    titleText.setFillColor(Color::Cyan);
-    messageText.setString("Press Enter to start!");
-    titleText.setString("< Timber >");
-    Vector2u size = window.getSize();
-    messageText.setPosition(size.x * 0.5f, size.y * 0.5f);
-    titleText.setPosition(size.x * 0.5f, size.y * 0.25f);
-    Utils::SetOrigin(messageText, Origins::MC);
-    Utils::SetOrigin(titleText, Origins::TC);
-    //
-    Text menuText;
-    menuText.setFont(font);
-    menuText.setCharacterSize(75);
-    menuText.setFillColor(Color::White);
-    menuText.setString("Menu\n1P\n2P");
-    menuText.setPosition(size.x * 0.5f, size.y * 0.5f);
-    Utils::SetOrigin(menuText, Origins::MC);
-    //
-=======
-    Vector2u size = window.getSize();
-
+    //Title
     um.SetTextUI("Press Enter to start!", "press enter");
     um.GetTextUI("press enter")->setPosition({ 500, 600 });
-    
     um.SetTextUI("Timber!", "title", 150, Color::Yellow);
     um.GetTextUI("title")->setPosition({ 700, 400 });
-    
+    //Menu
     um.SetTextUI("Menu");
-    um.GetTextUI("Menu")->setPosition({ size.x * 0.5f, size.y * 0.5f });
->>>>>>> 17c2a126f8e80abbdbd4babb06a339e066adf603
+    um.GetTextUI("Menu")->setPosition({ size.x * 0.35f, size.y * 0.15f });
+    um.GetTextUI("Menu")->setCharacterSize(225);
+    um.SetTextUI("1 player", "1p");
+    um.GetTextUI("1p")->setPosition({ size.x * 0.5f, size.y * 0.5f });
+    um.SetTextUI("2 players", "2p");
+    um.GetTextUI("2p")->setPosition({ size.x * 0.5f, size.y * 0.75f });
+    um.SetTextUI("->", "arrow");
+    um.GetTextUI("arrow")->setPosition({ size.x * 0.45f, size.y * 0.5f });
 
+    //instance
     Clock clock;
     bool SelectMenu = false;
+    bool SelectCharacter = false;
     bool PlayGame = false;
     bool Title = true;
     bool run = true;
-    int gamemode = 0;
+    int gamemode = 1;
+    int onePcharcter = 0;
+    int twoPcharcter = 0;
+
     while ( window.isOpen() )
     {
-        //sceneManager.Init();
         Time dt = clock.restart();
         Event ev;
 
@@ -121,16 +104,11 @@ int main()
             {
                 go->Draw(window);
             }
-<<<<<<< HEAD
-            window.draw(titleText);
-            window.draw(messageText);
-=======
+
             window.draw(*(um.GetTextUI("title")));
             window.draw(*(um.GetTextUI("press enter")));
->>>>>>> 17c2a126f8e80abbdbd4babb06a339e066adf603
             window.display();
         }
-
 
         //Menu Scene
         while ( SelectMenu )
@@ -145,6 +123,7 @@ int main()
 
             if ( InputManager::GetKeyDown(Keyboard::Key::Escape) )
             {
+                //gamemode = 0;
                 SelectMenu = false;
                 Title = true;
             }
@@ -156,14 +135,12 @@ int main()
             {
                 gamemode = 2;
             }
-            //Menu()
-            if ( Keyboard::isKeyPressed(Keyboard::Return) && gamemode != 0 )
+            if ( InputManager::GetKeyDown(Keyboard::Return) && gamemode != 0 )
             {
 
                 SelectMenu = false;
-                PlayGame = true;
+                SelectCharacter = true;
             }
-
 
             float deltaTime = dt.asSeconds();
 
@@ -182,20 +159,108 @@ int main()
             }
             if ( gamemode == 1 )
             {
-                um.GetTextUI("Menu")->setString("1P");
+                um.GetTextUI("arrow")->setPosition({ size.x * 0.45f, size.y * 0.5f });
             }
             else if( gamemode == 2 )
             {
-                um.GetTextUI("Menu")->setString("2P");
+                um.GetTextUI("arrow")->setPosition({ size.x * 0.45f, size.y * 0.75f });
             }
-            else
-            {
-                um.GetTextUI("Menu")->setString("Menu");
-            }
+            window.draw(*um.GetTextUI("arrow"));
             window.draw(*um.GetTextUI("Menu"));
+            window.draw(*(um.GetTextUI("1p")));
+            window.draw(*(um.GetTextUI("2p")));
             window.display();
         }
 
+        //Character select
+        while ( SelectCharacter )
+        {
+            Time dt = clock.restart();
+            Event ev;
+            InputManager::ClearInput();
+            while ( window.pollEvent(ev) )
+            {
+                InputManager::UpdateInput(ev);
+            }
+
+            if ( InputManager::GetKeyDown(Keyboard::Key::Escape) )
+            {
+                //gamemode = 0;
+                SelectCharacter = false;
+                SelectMenu = true;
+            }
+            if ( gamemode == 1 )
+            {
+                if ( InputManager::GetKeyDown(Keyboard::Key::A) && (onePcharcter < 0 || onePcharcter > 2) )
+                {
+                    onePcharcter -= 1;
+                }
+                if ( InputManager::GetKeyDown(Keyboard::Key::D) && (onePcharcter < 0 || onePcharcter > 2) )
+                {
+                    onePcharcter += 1;
+                }
+            }
+            else
+            {
+                if ( InputManager::GetKeyDown(Keyboard::Key::A) && (onePcharcter < 0 || onePcharcter > 2) )
+                {
+                    onePcharcter -= 1;
+                }
+                if ( InputManager::GetKeyDown(Keyboard::Key::D) && (onePcharcter < 0 || onePcharcter > 2) )
+                {
+                    onePcharcter += 1;
+                }
+                if ( InputManager::GetKeyDown(Keyboard::Key::Left) && (onePcharcter < 0 || onePcharcter > 2) )
+                {
+                    twoPcharcter -= 1;
+                }
+                if ( InputManager::GetKeyDown(Keyboard::Key::Right) && (onePcharcter < 0 || onePcharcter > 2) )
+                {
+                    twoPcharcter += 1;
+                }
+            }
+            
+            if ( InputManager::GetKeyDown(Keyboard::Return) && gamemode != 0 )
+            {
+                SelectMenu = false;
+                PlayGame = true;
+            }
+
+            float deltaTime = dt.asSeconds();
+
+            //Update
+            for ( auto go : gameObjectList )
+            {
+
+                go->Update(deltaTime);
+            }
+            
+            //Draw
+            window.clear();
+            for ( auto go : gameObjectList )
+            {
+                go->Draw(window);
+            }
+			for ( auto go : charcterList )
+			{
+				go->Draw(window);
+			}
+            /*if ( gamemode == 1 )
+            {
+                um.GetTextUI("arrow")->setPosition({ size.x * 0.45f, size.y * 0.5f });
+            }
+            else if ( gamemode == 2 )
+            {
+                um.GetTextUI("arrow")->setPosition({ size.x * 0.45f, size.y * 0.75f });
+            }*/
+            /*else
+            {
+                um.GetTextUI("Menu")->setString("Menu");
+            }*/
+            
+            window.display();
+        }
+       
         //Game Scene
         if( PlayGame && (gamemode == 1 || gamemode == 2))
         {
@@ -230,9 +295,16 @@ int main()
 
                 Event ev;
                 InputManager::ClearInput();
+
                 while ( window.pollEvent(ev) )
                 {
                     InputManager::UpdateInput(ev);
+                }
+                if ( InputManager::GetKeyDown(Keyboard::Key::Escape) )
+                {
+                    gamemode = 0;
+                    PlayGame = false;
+                    Title = true;
                 }
                 float deltaTime = dt.asSeconds(); //isPause ? 0.f : dt.asSeconds();
                 for ( auto i : gameObjectList )
@@ -245,8 +317,8 @@ int main()
                 }
                 window.display();
             }
-           
-        }       
+        } 
+
     }
     for ( auto go : gameObjectList )
     {
